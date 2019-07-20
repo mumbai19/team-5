560ef8c92bc1aed6d48f36dd6da8b9062e9ca7c3
DishaRajani <dishirajani1999@gmail.com>
2019-07-20 19:39:27 +0530

    Loginn

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'cfg' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'abc123' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
