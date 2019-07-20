a438590a343bd4131651dbe9e15c75b63998358d
Yogesh-1998 <yogeshmurarka777@gmail.com>
2019-07-21 02:09:27 +0530

    Added About US Page and event page

---
+++ front/About Us.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+
+body, html {
+  height: 100%;
+  margin: 0;
+  font: 400 15px/1.8 "Lato", sans-serif;
+  color: #777;
+}
+
+.bgimg-1, .bgimg-2, .bgimg-3 {
+  position: relative;
+  opacity: 0.65;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+.bgimg-1 {
+  background-image: url("img/img-1.jpg");
+  min-height: 100%;
+}
+
+.bgimg-2 {
+  background-image: url("img/img-2.jpg");
+  min-height: 400px;
+}
+
+.bgimg-3 {
+  background-image: url("img/img-3.jpg");
+  min-height: 400px;
+}
+
+.caption {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  color: #000;
+}
+
+.caption span.border {
+  background-color: #111;
+  color: #fff;
+  padding: 18px;
+  font-size: 25px;
+  letter-spacing: 10px;
+}
+
+h3 {
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font: 20px "Lato", sans-serif;
+  color: #111;
+}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1024px) {
+  .bgimg-1, .bgimg-2, .bgimg-3 {
+    background-attachment: scroll;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+  <span class="border">EVENTS</span>
+  </div>
+</div>
+
+<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3 style="text-align:center;">Who we are</h3>
+  <p>Trishul (NGO) is a registered charitable organization working towards building self-sustainable communities through skill development, education and livelihood programs in urban slums and economically weak rural villages in India. Core programs focus on empowering Women through skill development and empowering children’s through education.
+
+Environment Conservation through Up-cycle, Tree Plantation, Cleaning Forest Trails (Swachata Abhiyan) and Afforestation Projects.
+
+Over last two decades, we have impacted and helped improve the quality of life for more than 50,000 families in the surrounding areas via several projects like Trishul Women Empowerment Program; Trishul Learning Centre; BMC Dattakbasti Program (hygiene and sanitation, waste management etc), education, health and nutrition, disaster management, youth empowerment and environment conservation.
+</p>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+  <span class="border" style="font-size:25px;color: #f7f7f7;">What we do</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+  <!-- <p></p> -->
+  <div class="row">
+  <div class="col-md-6">
+    <ul class="">
+      <li><h4>Women Empowerment</h4></li>
+      <li><h4>Educational Scholarships</h4></li>
+      <li><h4>Protecting Environment</h4></li>
+    </ul>
+  </div>
+  <div class="col-md-6">
+    <ul>
+      <li><h4>Nutritional Education</h4></li>
+      <li><h4>Educating Underpriveledge</h4></li>
+      <li><h4>Protecting Environment</h4></li>
+    </ul>
+  </div>
+  </div>
+
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+  <span class="border" style="font-size:25px;color: #f7f7f7;">Registration Details</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    <ul>
+  <li>Registration as Charitable Society Maharashtra/1368/1996/GBBSD</li>
+  <li>Date of reg. – 17/10/1996 State where registered Maharashtra</li>
+  <li>Registration under societies registration act 1860</li>
+  <li>Registration as Charitable Trust Registration number F/19282 (Mumbai)</li>    
+  <li>Date of reg. - 04/08/1997 Registration under Bombay public trust act 1950</li>
+  <li>Income Tax Permanent Account No. (ITPAN) - AABTT3316K</li> 
+  <li>Registration–12AA (l)(b) w.e.f. 1.4.2007</li>
+  <li>80 G Registration No – 41635</li>
+  <li>NGO DARPAN ID: MH/2017/0172650</li>
+  </ul>
+  </div>
+</div>
+
+<div class="bgimg-1">
+  <div class="caption">
+  <span class="border">We believe in spreading love</span>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ front/eventDesc.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EventDesc</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container-fluid">
+		<h3 class="text-center">Upcoming Events</h3>
+		<hr>
+       <div class="row">	
+		<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+  			<img class="card-img-top" src="download.jpg" alt="Event">
+  			<div class="card-body">
+    		<h5 class="card-title">Fund Raising</h5>
+   			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Go somewhere</a>
+  		</div>
+  		</div>
+		</div>
+
+		<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+  			<img class="card-img-top" src="download.jpg" alt="Event">
+  			<div class="card-body">
+    		<h5 class="card-title">Fund Raising</h5>
+   			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Go somewhere</a>
+  		</div>
+  		</div>
+		</div>
+
+		<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+  			<img class="card-img-top" src="download.jpg" alt="Event">
+  			<div class="card-body">
+    		<h5 class="card-title">Fund Raising</h5>
+   			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Go somewhere</a>
+  		</div>
+  		</div>
+		</div>
+
+		<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+  			<img class="card-img-top" src="download.jpg" alt="Event">
+  			<div class="card-body">
+    		<h5 class="card-title">Fund Raising</h5>
+   			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Go somewhere</a>
+  		</div>
+  		</div>
+		</div>
+
+		<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+  			<img class="card-img-top" src="download.jpg" alt="Event">
+  			<div class="card-body">
+    		<h5 class="card-title">Fund Raising</h5>
+   			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Go somewhere</a>
+  		</div>
+  		</div>
+		</div>
+		
+
+	   </div>
+	</div>
+
+</body>
+</html>
