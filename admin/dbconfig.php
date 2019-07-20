7a4f7ae6b0a608283e3b86ed7b22c716051dfb70
Devang Chhajed <=>
2019-07-21 02:42:06 +0530

    Ecomm Devang

--- admin/dbconfig.php
+++ admin/dbconfig.php
@@ -1,3 +1,3 @@
 <?php 
-$con=mysqli_connect("localhost","root","","trishul");
+$con=mysqli_connect("localhost","root","abc123","trishul");
 ?>
---
+++ ecommdevang/Ecommerce Site PHP/ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2018 Brought to You By <a href="https://code-projects.org/">Code-Projects</a></strong>
+    </div>
+</footer>
