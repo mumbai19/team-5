a4f3b9956cfdf1d3c8bf8cea70b37b8bdae60e62
DishaRajani <dishirajani1999@gmail.com>
2019-07-21 08:01:12 +0530

    Report Generation

--- ecommdevang/EcommerceSitePHP/ecommerce/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>All rights reserved</b>
-      </div>
-      <strong>Copyright &copy; 2018 Brought to You By <a href="https://code-projects.org/">Code-Projects</a></strong>
-    </div>
-</footer>
